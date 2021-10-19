9454d1c036a25993c24e734984ef7e23f8bd6bdb
Per Olofsson <per.olofsson@assaabloy.com>
2021-10-19 14:29:54 +0200

    Loop Customfields

--- field.go
+++ field.go
@@ -1,6 +1,10 @@
 package jira
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"github.com/google/go-querystring/query"
+)
 
 // FieldService handles fields for the Jira instance / API.
 //
@@ -52,4 +56,72 @@ func (s *FieldService) GetListWithContext(ctx context.Context) ([]Field, *Respon
 // GetList wraps GetListWithContext using the background context.
 func (s *FieldService) GetList() ([]Field, *Response, error) {
 	return s.GetListWithContext(context.Background())
+}
+
+type FieldOptions struct {
+	// StartAt: The starting index of the returned projects. Base index: 0.
+	StartAt int `url:"startAt,omitempty"`
+	// MaxResults: The maximum number of projects to return per page. Default: 50.
+	MaxResults int `url:"maxResults,omitempty"`
+	// Expand: Expand specific sections in the returned issues
+	ProjectIds      string `url:"projectIds,omitempty"`
+	ScreenIds       string `url:"screenIds,omitempty"`
+	Types           string `url:"types,omitempty"`
+	LastValueUpdate int    `url:"lastValueUpdate,omitempty"`
+}
+
+type CustomFieldsType struct {
+	ID            string `json:"id"`
+	Name          string `json:"name"`
+	Description   string `json:"description,omitempty"`
+	Type          string `json:"type"`
+	SearcherKey   string `json:"searcherKey"`
+	Self          string `json:"self"`
+	NumericID     int    `json:"numericId"`
+	IsLocked      bool   `json:"isLocked"`
+	IsManaged     bool   `json:"isManaged"`
+	IsAllProjects bool   `json:"isAllProjects"`
+	ProjectsCount int    `json:"projectsCount"`
+	ScreensCount  int    `json:"screensCount"`
+}
+
+type CustomFieldsResponseType struct {
+	MaxResults int                `json:"maxResults"`
+	StartAt    int                `json:"startAt"`
+	Total      int                `json:"total"`
+	IsLast     bool               `json:"isLast"`
+	Values     []CustomFieldsType `json:"values"`
+}
+
+// Should be a separate file + ...
+//rest/api/2/customFields
+
+func (s *FieldService) GetAllCustomFieldsWithContext(ctx context.Context, options *FieldOptions) (*CustomFieldsResponseType, *Response, error) {
+	apiEndpoint := fmt.Sprintf("rest/api/2/customFields")
+	req, err := s.client.NewRequestWithContext(ctx, "GET", apiEndpoint, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if options != nil {
+		q, err := query.Values(options)
+		if err != nil {
+			return nil, nil, err
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+
+	issue := new(CustomFieldsResponseType)
+	resp, err := s.client.Do(req, issue)
+	if err != nil {
+		jerr := NewJiraError(resp, err)
+		return nil, resp, jerr
+	}
+
+	return issue, resp, nil
+}
+
+// Get wraps GetWithContext using the background context.
+func (s *FieldService) GetAllCustomFields(options *FieldOptions) (*CustomFieldsResponseType, *Response, error) {
+	return s.GetAllCustomFieldsWithContext(context.Background(), options)
 }
